a68fd5030d0514c70a3342ca987e90c9c3495a9f
hossidev <nsimba2025nwayami@gmail.com>
2026-09-25 13:26:31 +0100

    Aplicação envolvida no estado global da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import AppRoutes from "./routes/AppRoutes";
 import ScrollToTop from "./components/ScrollToTop";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./contexts/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ScrollToTop />
-      <Toaster position="top-right" containerStyle={{ zIndex: 10000 }} />
-      <AppRoutes />
+      <AuthProvider>
+        <ScrollToTop />
+        <Toaster position="top-right" containerStyle={{ zIndex: 10000 }} />
+        <AppRoutes />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
