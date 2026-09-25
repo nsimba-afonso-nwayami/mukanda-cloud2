b272436733210906e7f51d688134d28eb2668ae4
hossidev <nsimba2025nwayami@gmail.com>
2026-09-25 13:43:16 +0100

    Servidor proxy configurado

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [, tailwindcss(),
-    
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
-})
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://azulenovo.hossidev.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
